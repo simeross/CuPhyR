6222957e704fac33b3f35caf7075b3075c7e7713
simeon <simeon.rossmann@nibio.no>
2020-02-24 13:50:41 +0100

    substitute loop, fix 'NA' bug

--- R/abundant_tax_physeq.R
+++ R/abundant_tax_physeq.R
@@ -55,10 +55,13 @@ abundant_tax_physeq <- function(physeq = ps, lvl="Class", top=10, output_format=
   #Return based on output_format
   if(output_format=="ps"){
     #Loop through all elements in top_x and extract the ASVs corresponding to the top n taxgroups at lvl from the specified physeq object. Then prune physeq.
-    toptaxASVs <- character(0)
-    for (taxgroup in top_x){
-      toptaxASVs <- c(toptaxASVs, rownames(subset(phyloseq::tax_table(physeq), phyloseq::tax_table(physeq)[, lvl] == taxgroup)))
-    }
+    toptaxASVs <- ps_tbl %>%
+                  dplyr::group_by(OTU, !!lvlx) %>%
+                  dplyr::summarise() %>%
+                  dplyr::filter(!!lvlx %in% top_x) %>%
+                  dplyr::select(OTU) %>%
+                  unlist() %>%
+                  as.character()
     ps_topnt <- phyloseq::prune_taxa(toptaxASVs, physeq)
     return(ps_topnt)
   }
@@ -70,10 +73,13 @@ abundant_tax_physeq <- function(physeq = ps, lvl="Class", top=10, output_format=
     warning("Choose \"ps\" or \"tops\" for output_format! ", output_format, " is not a legitimate value.
             Phyloseq object returned as default")
     #Loop through all elements in top_x and extract the ASVs corresponding to the top n taxgroups at lvl from the specified physeq object. Then prune physeq.
-    toptaxASVs <- character(0)
-    for (taxgroup in top_x){
-      toptaxASVs <- c(toptaxASVs, rownames(subset(phyloseq::tax_table(physeq), phyloseq::tax_table(physeq)[, lvl] == taxgroup)))
-    }
+    toptaxASVs <- ps_tbl %>%
+                  dplyr::group_by(OTU, !!lvlx) %>%
+                  dplyr::summarise() %>%
+                  dplyr::filter(!!lvlx %in% top_x) %>%
+                  dplyr::select(OTU) %>%
+                  unlist() %>%
+                  as.character()
     ps_topnt <- phyloseq::prune_taxa(toptaxASVs, physeq)
     return(ps_topnt)
   }
